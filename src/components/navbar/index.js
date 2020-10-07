d7121940f8b491ec45d4bdb7a460091e6f1d47aa
Carlos Aviles Buitron <admin@carlos-developer.com>
2020-10-07 11:14:31 -0400

    Adding yelp reviews

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -14,8 +14,9 @@ const NavBar = ()=>{
             <ul>
                 <li><NavLink exact to="/" activeStyle={{color: "#f03c02"}}>Home</NavLink></li>
                 <li><NavLink to="/about" activeStyle={{color: "#f03c02"}}>About</NavLink></li>
-                <li><NavLink to="/gallery" activeStyle={{color: "#f03c02"}}>Gallery</NavLink></li>
+                <li className="mobile-off"><NavLink to="/gallery" activeStyle={{color: "#f03c02"}}>Gallery</NavLink></li>
                 <li><NavLink to="/contact" activeStyle={{color: "#f03c02"}}>Contact</NavLink></li>
+                <li><a href="https://www.yelp.com/biz/sophys-upholstery-whitestone" target="_blank" rel="noopener noreferrer">Reviews</a></li>
                 <li><a href="https://www.facebook.com/sophys.uphosltery" target="_blank" rel="noopener noreferrer">Facebook</a></li>
             </ul>
         </nav>
